069f02e1843d003b3abad479799db10201273f38
Shoichi Okada <okada-s@klab.com>
2014-12-12 19:50:12 +0900

    時針の動きを調整

--- ClockAddIn/MyControl.xaml.cs
+++ ClockAddIn/MyControl.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Company.ClockAddIn
 {
@@ -30,7 +19,7 @@ namespace Company.ClockAddIn
                                                                                {
                                                                                    var now = DateTime.Now;
                                                                                    this.nowTimeBlock.Text = now.ToString("HH:mm");
-                                                                                   this.hoursRotation.Angle = Math.Floor(now.Hour / 12d * 360d);
+                                                                                   this.hoursRotation.Angle = (now.Hour * (360d / 12d)) + (now.Minute * (30d / 60d));
                                                                                    this.minutesRotation.Angle = Math.Floor(now.Minute / 60d * 360d);
                                                                                })), null, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
         }
